140ebf03e33df6cf2bfd67fbd020488e4d8cbcc2
Zanoni01 <andrezanoni01@gmail.com>
2026-09-05 17:45:54 -0300

    Atualizar cache para o Roteiro do Plantão

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = "sopz-v2";
+const CACHE = "sopz-v3";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
